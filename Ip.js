ed946a1646bb5b7655640f34d206d040e858cc52
gtlee <56505533+miudhheb@users.noreply.github.com>
2019-10-24 12:23:10 +0800

    Update Ip.js

--- Ip.js
+++ Ip.js
@@ -33,5 +33,4 @@ var body = $response.body;
 var obj = JSON.parse(body);
 var title =flags.get(obj['countryCode']) + ' '+ obj['country'];
 var ip = obj['query'];
-var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
 $done({title, subtitle, ip, description});
