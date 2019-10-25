2102a93700ae365846fbf34bc48e467c4a3a3f32
gtlee <56505533+miudhheb@users.noreply.github.com>
2019-10-25 11:56:26 +0800

    Update Ip.js

--- Ip.js
+++ Ip.js
@@ -15,8 +15,9 @@ function getRandomInt(max) {
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title =obj['country'] + '-' +obj['city'];
-var subtitle = '('+obj['ipType']+'
+var title =obj['country']+ '-' +obj['city'];
+var subtitle = obj['isp']+ '('+obj['ipType']+')';
+var ip = obj['query'];
 var description = obj['country'] + '-' + obj['city'] + '\n' + obj['isp'] + '\n' + obj['ipType']+ '\n' + ip;
 
 $done({title, subtitle, ip, description});
