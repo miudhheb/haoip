06d749455afb77bc6b60c7449654e6984730c926
gtlee <56505533+miudhheb@users.noreply.github.com>
2019-10-25 12:09:44 +0800

    Update Ip.js

--- Ip.js
+++ Ip.js
@@ -18,7 +18,7 @@ var obj = JSON.parse(body);
 var title =obj['country'];
 var description = obj['country'];
 var subtitle ='('+obj['ipType']+')';
-var ip = obj['query'];
+var ip =obj['isp']+  obj['query'];
 var description = obj['country'] + '-' + obj['city'] + '\n' + obj['isp'] + '\n' + obj['ipType']+ '\n' + ip;
 
 $done({title, subtitle, ip, description});
