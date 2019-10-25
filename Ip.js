87bac724f259a1202b60931b14605bb1473d2e5f
gtlee <56505533+miudhheb@users.noreply.github.com>
2019-10-25 11:42:18 +0800

    Update Ip.js

--- Ip.js
+++ Ip.js
@@ -4,12 +4,20 @@
 
 // var body = $response.body;
 // var obj = JSON.parse(body);
-var flags = new 
+
+if ($response.statusCode != 200) {
+  $done(Null);
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 var body = $response.body;
 var obj = JSON.parse(body);
-var title = flags.get(obj['country_iso']) + ' '+ obj['country'];
-var subtitle =ValidCheck(obj['city'])+'-'+'('+obj['asn_org']+');
-var ip = obj['ip'];
-var description = obj['country'] + '-' +ValidCheck(obj['city']) + '\n' + obj['asn'] + '\n' + obj['asn_org']+ '\n' + obj['ip'];
+var title =obj['country']+ '-' +obj['city'];
+var subtitle =obj['isp']+ '('+obj['ipType']+')';
+var ip = obj['query'];
+var description = obj['country'] + '-' + obj['city'] + '\n' + obj['isp'] + '\n' + obj['ipType']+ '\n' + ip;
 
 $done({title, subtitle, ip, description});
