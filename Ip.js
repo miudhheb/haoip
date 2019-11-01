c4b5444a4d696f5c6c9a6a8d710b904f8e284f72
gtlee <56505533+miudhheb@users.noreply.github.com>
2019-11-01 16:29:25 +0800

    Update Ip.js

--- Ip.js
+++ Ip.js
@@ -17,7 +17,7 @@ var body = $response.body;
 var obj = JSON.parse(body);
 var title =obj['country'];
 var description =obj['country'];
-var subtitle ='国际专线(IPLC)－剩余流量：612G';
+var subtitle ='国际专线(IPLC)－剩余流量：612.59g';
 var ip = obj['query'];
 var description = obj['country'] + '-' + obj['city'] + '\n' + obj['isp'] + '\n' + obj['ipType']+ '\n' + ip;
 
